7fd841d1670db25b85a1f76d4463b7b351ecf5d7
saketh-26 <sakethreddy.kallepu@gmail.com>
2026-07-25 05:59:44 +0000

    Final Patterns

--- Control_Statements.py
+++ Control_Statements.py
@@ -114,6 +114,56 @@ A
 B C
 D E F
 G H I J
-test working
-'''
 
+#get the below pattern
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
+A
+A B
+A B C
+A B C D
+A B C D E
+
+for i in range(65,70):
+    for j in range(65,i+1):
+        print(chr(j),end=' ')
+    print()
+
+Get Star Pyramid Pattern
+
+        *
+       * *
+      * * *
+     * * * *
+    * * * * *
+    
+n = int(input("Enter a value:"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        #print(i,j)
+        if j <= n-i:
+            #print(i,j)
+            print(" ",end='')
+        else:
+            print('*',end= " ")
+    print()
+#Home task Print Diamond pattern
+        *
+       * *
+      * * *
+     * * * *
+    * * * * *
+     * * * *
+      * * *
+       * *
+        *
+'''
